0fd482f9b2632267e517e923e22f86c30a9df443
snappykey <snappykey@gmail.com>
2025-05-14 18:20:10 +0300

    Add files via upload

---
+++ v1_3x4/config.h.cs
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "quantum.h"
+
+#define DIP_SWITCH_PINS { B6, F6 }
+// #define switch1  B6
+// #define switch2  F6
+#define LED1 F7
+#define LED2 B2
+#define LED3 B5
---
+++ v1_3x4/snappypad/v1_3x4/config.h.cs
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "quantum.h"
+
+#define DIP_SWITCH_PINS { B6, F6 }
+// #define switch1  B6
+// #define switch2  F6
+#define ENCODERS_PAD_A { B1 } // replace with your actual pin
+#define ENCODERS_PAD_B { B2 } 
+
+#define LED1 F7
+#define LED2 B2
+#define LED3 B5
